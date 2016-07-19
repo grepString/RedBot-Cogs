d63cdb840d40622029b01b146b45a6980d9ab8c6
grepString <grepString@users.noreply.github.com>
2016-07-19 17:20:36 -0400

    Initial

---
+++ steam.py
@@ -0,0 +1,23 @@
+import discord
+from discord.ext import commands
+import json
+import requests
+
+class Steam:
+
+    """Steam Connect Protocol"""
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def connect(self, ip : str):
+        """Creates a URL to connect
+        to a Steam game server.
+        Example: !connect 12.34.56.78:27015"""
+
+        url = "steam://connect/"+ip
+        await self.bot.say(url)
+
+def setup(bot):
+    bot.add_cog(Steam(bot))
